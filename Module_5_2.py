e3f90c11f240610751610dd8f5666d6787dc733a
VasilyDrozhzhin <vasilydrozhzhin@gmail.com>
2024-11-08 13:35:14 +0300

    Add files via upload

--- Module_5_2.py
+++ Module_5_2.py
@@ -1,12 +1,23 @@
-# https://github.com/VasilyDrozhzhin/Lesson0/blob/main/Module_5_2.py
+# Добрый день! Добавил метод из предыдущего решения и также создал новый репозиторий для текущего модуля
+
 class House:
 	def __init__(self, name, number_of_floors):
 		self.name = name
 		self.number_of_floors = number_of_floors
+
+	def go_to(self, new_floor):
+		if 1 > new_floor or new_floor > self.number_of_floors:
+			print('Такого этажа не существует')
+		else:
+			for i in range(1, new_floor + 1):
+				print(i)
+
 	def __len__(self):
 		return self.number_of_floors
+
 	def __str__(self):
 		return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
